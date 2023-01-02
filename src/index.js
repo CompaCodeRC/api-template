100a7a8155de005770c137dc9998dab829331eaa
CompaCode <rondemon18@gmail.com>
2023-01-02 02:43:16 -0500

    [Change] Mensaje de inicio

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './databases/mongodb';
 import config from './config';
 app.listen(config.PORT);
 
-console.log(`Server running on port ${config.PORT}`);
+console.log(`Servidor iniciado en el puerto: ${config.PORT}`);
