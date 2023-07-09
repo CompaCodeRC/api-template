f7783a719aa8ead8e7a1f0cacb0ed9523a30af74
CompaCode <rondemon18@gmail.com>
2023-07-09 08:21:55 -0500

    [Fix] Console Port

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './install';
 import './cronjobs';
 
 import config from './config';
-console.log('port', process.env.PORT);
 const server = app.listen(process.env.PORT || config.PORT, () => {
     console.log('Listening on port:', server.address().port);
 });
