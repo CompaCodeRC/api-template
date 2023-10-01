12c6ffde0cece3e61239cc8685d9da866459d2f2
CompaCode <rondemon18@gmail.com>
2023-09-30 19:16:28 -0500

    [Fix] Folder name

--- src/install/LocalData.js
+++ src/install/LocalData.js
@@ -1,15 +1,15 @@
 import fs from "fs";
 
 export default async function LocalData() {
-    if (!fs.existsSync('./src/database/local_data')) {
-        fs.mkdirSync('./src/database/local_data');
+    if (!fs.existsSync('./src/databases/local_data')) {
+        fs.mkdirSync('./src/databases/local_data');
     }
     
-    if (!fs.existsSync('./src/database/local_data/prices.json')) {
-        fs.writeFileSync('./src/database/local_data/prices.json', '[]');
+    if (!fs.existsSync('./src/databases/local_data/prices.json')) {
+        fs.writeFileSync('./src/databases/local_data/prices.json', '[]');
     }
 
-    if (!fs.existsSync('./src/database/local_data/ticker.json')) {
-        fs.writeFileSync('./src/database/local_data/ticker.json', '[]');
+    if (!fs.existsSync('./src/databases/local_data/ticker.json')) {
+        fs.writeFileSync('./src/databases/local_data/ticker.json', '[]');
     }
 };
